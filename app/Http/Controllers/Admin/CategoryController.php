66ac6195be8d3faa363113adaf3e13a54e8be32e
Vyom Patel <vyomp59@gmail.com>
2024-11-06 19:02:32 +0530

    active inactive and delete functionality

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,8 +4,58 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\AdminsRole;
+use Auth;
 
 class CategoryController extends Controller
 {
-    //
+    public function categories() {
+        $categories = Category::with('parentCategory')->get()->toArray();
+        $categories = json_decode(json_encode($categories), true);
+
+        $admin_id = Auth::guard('admin')->user()->id;
+        $module_count = AdminsRole::where(['subadmin_id' => $admin_id, 'module' => 'categories'])->count();
+
+        if (Auth::guard('admin')->user()->type == 'admin') {
+            $categories_module['view_access'] = 1;
+            $categories_module['edit_access'] = 1;
+            $categories_module['full_access'] = 1;
+        } else {
+            if ($module_count == 0) {
+                $message = 'This feature is restricted to this Sub Admins';
+                return redirect('admin/dashboard')->with('error_message', $message);
+            }
+
+            $categories_module = AdminsRole::where(['subadmin_id' => $admin_id, 'module' => 'categories'])->first();
+
+            if ($categories_module->view_access == 0 && $categories_module->edit_access == 0 && $categories_module->full_access == 0) {
+                $message = 'This feature is restricted to this Sub Admins';
+                return redirect('admin/dashboard')->with('error_message', $message);
+            }
+
+            $categories_module = $categories_module->toArray();
+        }
+
+        return view('admin.categories.categories')->with(compact('categories', 'categories_module'));
+    }
+
+    public function updateCategoryStatus(Request $request) {
+        if ($request->ajax()) {
+            $data = $request->all();
+            if ($data['status'] == 'Active') {
+                $status = 0;
+            } else {
+                $status = 1;
+            }
+
+            Category::where('id', $data['category_id'])->update(['status' => $status]);
+            return response()->json(['status'=>$status, 'category_id'=>$data['category_id']]);
+        }
+    }
+
+    public function deleteCategory($id) {
+            Category::where('id', $id)->delete();
+            return response()->back()->with('success_message', 'category deleted successfully!!');
+    }
 }
---
+++ resources/views/admin/categories/categories.blade.php
@@ -0,0 +1,109 @@
+@extends('admin.layouts.layout')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Categories Management</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                            <li class="breadcrumb-item active">Categories</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Categories</h3>
+                                @if ($categories_module['edit_access'] == 1 || $categories_module['full_access'] == 1)
+                                    <a style="max-width: 150px; float: right; display:inline-block"
+                                        href="{{ url('admin/add-edit-cms-page') }}" class="btn btn-block btn-primary">Add
+                                        Category</a>
+                                @endif
+                            </div>
+                            @if (Session::has('success_message'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    <strong>Success!</strong>{{ Session::get('success_message') }}
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                            @endif
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="categories" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Name</th>
+                                            <th>Parent Category</th>
+                                            <th>URL</th>
+                                            <th>Created on</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($categories as $category)
+                                            <tr>
+                                                <td>{{ $category['id'] }}</td>
+                                                <td>{{ $category['category_name'] }}</td>
+                                                <td>
+                                                    @if (isset($category['parentCategory']['category_name']))
+                                                        {{ $category['parentCategory']['category_name'] }}
+                                                    @else
+                                                        -
+                                                    @endif
+                                                </td>
+                                                <td>{{ $category['url'] }}</td>
+                                                <td>{{ date('F j, Y, g:i a', strtotime($category['created_at'])) }}</td>
+                                                <td>
+                                                    @if ($category['status'] == 1)
+                                                        <a class="updateCategoryStatus" id="category-{{ $category['id'] }}"
+                                                            category_id="{{ $category['id'] }}" href="javascript:void(0)"
+                                                            style="color:#3f6ed3"><i class="fas fa-toggle-on"
+                                                                status="Active"></i></a>
+                                                    @else
+                                                        <a class="updateCategoryStatus" id="category-{{ $category['id'] }}"
+                                                            category_id="{{ $category['id'] }}" href="javascript:void(0)"
+                                                            style="color:grey"><i class="fas fa-toggle-off"
+                                                                status="Inactive"></i></a>
+                                                    @endif
+                                                    &nbsp;&nbsp;
+                                                    <a style="color:#3f6ed3" class="confirmDelete" title="Delete Category"
+                                                        href="javascript:void(0)" record="category"
+                                                        recordid="{{ $category['id'] }}"><i class="fas fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+
+
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
