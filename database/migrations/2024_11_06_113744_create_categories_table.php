2b465394fdcb5e0fb6eee77005c130208b13d90c
Vyom Patel <vyomp59@gmail.com>
2024-11-06 17:39:46 +0530

    insert categories

---
+++ database/migrations/2024_11_06_113744_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('parent_id')->nullable();
+            $table->string('category_name');
+            $table->string('category_image');
+            $table->float('category_discount')->nullable();
+            $table->text('description');
+            $table->string('url');
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->string('meta_keywords')->nullable();
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
